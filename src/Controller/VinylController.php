857b0994c1440c192304311860eea98e843b6f24
Xenus3 <k.aberbach@hotmail.com>
2024-07-15 16:37:13 +0200

    twig2

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,15 +4,31 @@
 namespace App\Controller;
 
 use LDAP\Result;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VinylController
+class VinylController extends AbstractController
 {
+
+    private $tracks = [
+        ['song' => 'Gangsta Paradide', 'artist' => 'Coolio'],
+        ['song' =>'Waterfalls', 'artist' => 'TLC'],
+        ['song' =>'Creep' , 'artist' => 'Radiohead'],
+        ['song' =>'Kiss from a Rose' , 'artist' => 'Seal'],
+        ['song' =>'On Bended Knee' , 'artist' => 'Boyz II Men'],
+        ['song' =>'Fantasy' , 'artist' => 'Mariah Carey']
+    ];
+
+
     #[Route('/')]
     public function homepage() : Response 
     {
-       return new Response("Title: PB and Jams");
+       //return new Response("Title: PB and Jams");
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $this->tracks,
+        ]);
     }
 
     #[Route('/browse/{musicCategory}')]
